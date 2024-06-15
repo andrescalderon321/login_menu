f445177f90ade97d09ed829fdfef12891b6c7470
andrescalderon321 <andresfernandoc90@gmail.com>
2024-06-14 23:31:59 -0500

    tercer commit se creo migraciones y modelos

---
+++ database/migrations/2024_06_14_231918_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('estado');
+            $table->timestamp('fecha de pedido');
+
+            //foranea de tables
+
+            $table->unsignedBigInteger('table_id');
+
+            $table->foreign('table_id')
+            ->references('id')
+            ->on('tables')
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
